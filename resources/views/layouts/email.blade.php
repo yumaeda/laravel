5935b5f9109b7d5764fc38a697e27efbcb367623
Yukitaka Maeda <yumaeda@gmail.com>
2018-04-19 00:21:16 +0900

    Add image to email template

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -1,4 +1,7 @@
 <h3>@yield('title')</h3>
+<div style="margin-top: 20px; width: 100%">
+    <img style="padding: 15px; width: 100%;" src="https://s3-ap-northeast-1.amazonaws.com/matsune-bank-s3/images/email/mail_img.png" alt="Main Image">
+</div>
 <div>
     @yield('content')
 </div>
