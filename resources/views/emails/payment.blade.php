d90f2b33e77d4dc5ff4e18b90af4ae4496725f01
Yukitaka Maeda <yumaeda@gmail.com>
2018-05-07 23:08:29 +0900

    Email: Update payment template

--- resources/views/emails/payment.blade.php
+++ resources/views/emails/payment.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.email')
 
 @section('title')
-    {{ $store_name }}
+    Store:&nbsp;{{ $store_name }}
 @endsection
 
 @section('content')
-    {{ $donner->first_name }}&nbsp;{{ $donner->last_name }}
+    &gt;&gt;&gt;&nbsp;{{ $donner->first_name }}&nbsp;{{ $donner->last_name }}
     <br><br>
     {{ $point }} points were withdrawn from your account.
 @endsection
