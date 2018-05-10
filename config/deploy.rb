666c39e7f39f9d4b60be70d429a9f29909b1909f
Yukitaka Maeda <yumaeda@gmail.com>
2018-05-10 23:17:50 +0900

    Capistrano: Update configuration file

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "Matsune-Family"
 set :repo_url, "https://github.com/yumaeda/matsune-laravel.git"
 
 # Default branch is :master
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, "master"
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/matsune"
@@ -21,10 +21,10 @@ set :deploy_to, "/var/www/matsune"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, ".env"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "storage/logs"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
